9a50bd5fc3425c6f4c9c2a32ebb2bf3f55c6e69b
Rohan Cribbs <rohan.cribbs@thoughtforger.com>
2018-11-29 14:11:09 -0800

    Change travis pushback script to be a little more sensible.

--- .travis/pushback.sh
+++ .travis/pushback.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
-add_changes_if_exist() {
-  echo "Checking for changes in generated code..."
-  if git diff-index --quiet HEAD --; then
-    echo "Nothing to commit, skipping push-back."
-  else
-    echo "Found changes, commiting them back to repo..."
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "Travis CI"
-    git remote set-url origin \
-        https://${GH_TOKEN}@github.com/DaiHasso/certy.git > /dev/null 2>&1
-    git fetch
-    git add generated_certs.go .certshash
-    git commit --message "Automated Travis Build: $TRAVIS_BUILD_NUMBER"
-    git push origin HEAD:master
-    echo "Finished pushing back changes to GitHub."
-  fi
+add_changes() {
+  echo "Committing any changes back to repo..."
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git remote set-url origin \
+      https://${GH_TOKEN}@github.com/DaiHasso/certy.git > /dev/null 2>&1
+  git fetch
+  git add generated_certs.go .certshash
+  git commit --message "Automated Travis Build: $TRAVIS_BUILD_NUMBER"
+  git push origin HEAD:master
+  echo "Finished pushing back changes to GitHub."
 }
 
-add_changes_if_exist
+add_changes
