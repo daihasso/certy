586cd76f2c5885e32999388bc83836717c18f2b9
Rohan Cribbs <rohan.cribbs@thoughtforger.com>
2018-11-29 00:02:18 -0800

    Update script again, hopefully it works this time.

--- .travis/pushback.sh
+++ .travis/pushback.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 add_changes_if_exist() {
-  git remote add origin-auto \
-      https://${GH_TOKEN}@github.com/DaiHasso/certy.git > /dev/null 2>&1
-  git fetch
   echo "Checking for changes in generated code..."
-  if git diff-index --quiet remotes/origin-auto/master --; then
+  if git diff-index --quiet HEAD --; then
     echo "Nothing to commit, skipping push-back."
   else
+    echo "Found changes, commiting them back to repo..."
+    git remote add origin-auto \
+        https://${GH_TOKEN}@github.com/DaiHasso/certy.git > /dev/null 2>&1
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
     git add generated_certs.go .certshash
     git commit --message "Automated Travis Build: $TRAVIS_BUILD_NUMBER"
-    git push origin-auto master
+    git push -u origin-auto master
     echo "Finished pushing back changes to GitHub."
   fi
 }
