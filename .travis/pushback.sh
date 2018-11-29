88ab0cce0f2c6ff1396f7ffedff857ae89c6916b
Rohan Cribbs <rohan.cribbs@thoughtforger.com>
2018-11-28 23:52:02 -0800

    Fetch better.

--- .travis/pushback.sh
+++ .travis/pushback.sh
@@ -3,7 +3,7 @@
 add_changes_if_exist() {
   git remote add origin-auto \
       https://${GH_TOKEN}@github.com/DaiHasso/certy.git > /dev/null 2>&1
-  git fetch origin-auto master
+  git fetch
   echo "Checking for changes in generated code..."
   if git diff-index --quiet remotes/origin-auto/master --; then
     echo "Nothing to commit, skipping push-back."
@@ -12,7 +12,7 @@ add_changes_if_exist() {
     git config --global user.name "Travis CI"
     git add generated_certs.go .certshash
     git commit --message "Automated Travis Build: $TRAVIS_BUILD_NUMBER"
-    git push --set-upstream origin-auto master
+    git push origin-auto master
     echo "Finished pushing back changes to GitHub."
   fi
 }
